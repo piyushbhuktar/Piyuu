6af10936e350169960bdad6701bd61ae5d654b0e
piyushbhuktar <piyushbhuktar@gmail.com>
2026-07-19 00:34:06 +0530

    Update login.js

--- admin/js/login.js
+++ admin/js/login.js
@@ -42,7 +42,7 @@ document.addEventListener("DOMContentLoaded", () => {
         };
 
         try {
-            const response = await fetch("http://localhost:5000/api/login", {
+            const response = await fetch("https://piyuu-ripg.onrender.com/api/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
